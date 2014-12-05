7e533c708c6b285282f909d8ff3f320626e738f3
Antoine Rougier <rougier@par.tuwien.ac.at>
2014-12-05 10:56:10 +0100

    merging …

--- Exercise 1/communities/cc-lr.R
+++ Exercise 1/communities/cc-lr.R
@@ -1,2 +1,11 @@
+## Owner: Antoine Rougier, 4 Dec. 2014
+## Dataset: http://archive.ics.uci.edu/ml/datasets/Communities+and+Crime
 
-cc <- read.csv("communities.data")
+
+cc.set <- read.csv("communities.data", na.strings = "?", header = FALSE)
+
+## Create a formula for a model with a large number of variables:
+cc.vars<- names(cc.set[1:127])
+cc.formula <- as.formula(paste("V128 ~ ", paste(cc.vars, collapse= "+")))
+
+cc.lm <- lm(formula = cc.formula, data = cc.set)
