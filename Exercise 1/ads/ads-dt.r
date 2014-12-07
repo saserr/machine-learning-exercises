acdd5c45b6509e8798f31623123441e710b65dd7
Sanjin Sehic <saserr@gmail.com>
2014-12-07 17:23:12 +0100

    Update decision tree classification for ads

--- Exercise 1/ads/ads-dt.r
+++ Exercise 1/ads/ads-dt.r
@@ -1,7 +1,7 @@
 require('rpart')
 
 # read csv file
-ads <- read.csv('ads/ad.train.data', header = TRUE, sep = ',', na.strings = '?')
+ads <- read.csv('ads/ad.data', header = TRUE, sep = ',', na.strings = '?')
 
 # cleanup data
 ads$V1559 <- factor(as.numeric(ads$V1559 == 'ad.'), labels = c('no', 'yes'))
@@ -17,14 +17,15 @@ ads_miss_as_mean$V2[is.na(ads_miss_as_mean$V2)] <- as.integer(mean(x = ads$V2, n
 ads_miss_as_mean$V3[is.na(ads_miss_as_mean$V3)] <- mean(x = ads$V3, na.rm = TRUE)
 ads_miss_as_mean$V4[is.na(ads_miss_as_mean$V4)] <- 1
 
-# grow a tree
+# grow and prune the tree
 fit <- rpart(V1559 ~ .,
              data = ads_miss_as_mean,
              method = 'class',
-             control = rpart.control(xval = 10, minsplit = 10, cp = 0.001))
+             control = rpart.control(xval = 5, cp = 0))
+ads_dt_mean_5 <- prune(fit, cp = fit$cptable[which.min(fit$cptable[,'xerror']), 'CP'])
 
 # print information about the tree
-printcp(fit)
+printcp(ads_dt_mean_5)
 summary(fit, c = 0.01)
 plotcp(fit)
 
@@ -32,16 +33,3 @@ plotcp(fit)
 plot(fit, uniform = TRUE, main = 'Classification Tree for Ads')
 text(fit, use.n = FALSE, all = TRUE, cex = .8)
 post(fit, use.n = FALSE, file = 'ads-tree.ps', title = 'Classification Tree for Ads')
-
-# prune the tree
-pfit<- prune(fit, cp = fit$cptable[which.min(fit$cptable[,'xerror']), 'CP'])
-
-# print information about the pruned tree
-printcp(pfit)
-summary(pfit, c = 0.01)
-plotcp(pfit)
-
-# plot the pruned tree
-plot(fit, uniform = TRUE, main = 'Classification Tree for Ads')
-text(fit, use.n = FALSE, all = TRUE, cex = .8)
-post(fit, use.n = FALSE, file = 'ads-pruned-tree.ps', title = 'Classification Tree for Ads')
