14dae53947c55fcfeb200cbeae2df2a73874f1bb
Sanjin Sehic <saserr@gmail.com>
2014-12-09 11:50:31 +0100

    Update linear regression for solar flares

--- Exercise 1/flares/flares-lm.r
+++ Exercise 1/flares/flares-lm.r
@@ -6,24 +6,12 @@
 library(ggplot2)
 library(DAAG)
 
-flares.set <- read.csv('./flares.csv', header = TRUE, sep = ' ')
+flares <- read.csv('flares/flares.csv', header = TRUE, sep = ' ')
 
-# Fit linear regression, let's take the full model
-flares.lm <- lm(c.class ~ ., data = flares.set)
-
-# sum up
-summary(flares.lm)
-confint(flares.lm, level=0.95) # Confidence interval
-
-# diagnostic plots
-layout(matrix(c(1,2,3,4),2,2))
-plot(flares.lm)
-
-# K fold cross-validation (k >1)
-cv.lm(df = flares.set, flares.lm, m = 3) 
-
-
-# Prediction
-# Shortcoming because of sigularities, coefficients are not set
-p <- predict(flares.lm, flares[-(1:100),]) 
-1 - sum(p^2) / sum((flares.set$c.class - mean(flares.set$c.class))^2)
+# fit linear regression
+fit <- lm(x.class ~ code.class + code.largest.spot.size + code.spot.distribution + activity + evolution + code.24h.activity + historically.complex + bacame.historically.complex.now + area + largest.spot.area,
+          data = flares)
+flares_lr_x <- cv.lm(fit,
+                     df = flares,
+                     m = 5,
+                     seed = 5)
